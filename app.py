6f223aaa72d70886b2c562901fc991f9635c0159
duaneywadey <idrdequito.cvt@eac.edu.ph>
2022-11-11 14:33:20 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+
+
+model = joblib.load('house_price_ph_model (1)')
+
+app = Flask(__name__)
+app.debug = True
+
+# A must at all times
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/')
+def indexPage():
+	
+	# The form is here
+    return render_template('index.html')
+
+@app.route('/result' , methods=['POST', 'GET'])
+def resultPage():
+	a = request.form['a']
+	b = request.form['b']
+	c = request.form['c']
+
+	arr = np.array([[a,b,c]], dtype=float)
+	pred = model.predict(arr)
+	predInt = int(pred)
+
+	return render_template('result.html', data=predInt)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
