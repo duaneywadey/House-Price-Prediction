2c25823d6b520a9bcdf372d15c655001462fd4de
duaneywadey <idrdequito.cvt@eac.edu.ph>
2022-11-12 12:16:29 +0800

    added new feature

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import joblib
 import numpy as np
 
 
-model = joblib.load('house_price_ph_model (1)')
+model = joblib.load('with_labels_house_price_ph')
 
 app = Flask(__name__)
 app.debug = True
@@ -18,15 +18,60 @@ def indexPage():
 	# The form is here
     return render_template('index.html')
 
+# Batangas	Bulacan	Cavite	Laguna	Manila	Pampanga	Quezon	Rizal
 @app.route('/result' , methods=['POST', 'GET'])
 def resultPage():
 	a = request.form['a']
 	b = request.form['b']
 	c = request.form['c']
+	d = int(request.form['d'])
 
-	arr = np.array([[a,b,c]], dtype=float)
-	pred = model.predict(arr)
-	predInt = int(pred)
+	print(d)
+
+	if d==1:
+		arr = np.array([[a,b,c,1,0,0,0,0,0,0,0]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
+
+	elif d==2:
+		arr = np.array([[a,b,c,0,1,0,0,0,0,0,0]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
+
+	elif d==3:
+		arr = np.array([[a,b,c,0,0,1,0,0,0,0,0]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
+
+	elif d==4:
+		arr = np.array([[a,b,c,0,0,0,1,0,0,0,0]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
+
+	elif d==5:
+		arr = np.array([[a,b,c,0,0,0,0,1,0,0,0]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
+
+	elif d==6:
+		arr = np.array([[a,b,c,0,0,0,0,0,1,0,0]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
+
+	elif d==7:
+		arr = np.array([[a,b,c,0,0,0,0,0,0,1,0]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
+
+	elif d==8:
+		arr = np.array([[a,b,c,0,0,0,0,0,0,0,1]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
+
+	else:
+		arr = np.array([[a,b,c,0,0,0,0,0,0,0,0]], dtype=float)
+		pred = model.predict(arr)
+		predInt = int(pred)
 
 	return render_template('result.html', data=predInt)
 
